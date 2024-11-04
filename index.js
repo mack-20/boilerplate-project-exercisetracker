095276d22690d2b1b8d7333460dc658b5dd24d59
vader066 <humphreydonkor066@gmail.com>
2024-11-04 17:20:43 +0000

    Set up POST and GET route on /api/users

--- index.js
+++ index.js
@@ -14,13 +14,42 @@ app.get('/', (req, res) => {
 });
 app.use(express.json())
 
+// Connect to the MongoDB 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // Set POST route at /api/users
-app.post('/api/users', (req, res) => {
-  const user = new User({
-    username: req.body.username
-  })
-  
+app.post('/api/users', async (req, res) => {
+  try{
+    const username = req.body.username
+
+    const user = await User.create({username: username})
+
+    const user_id = user._id
+
+    res.json({
+      username: username,
+      _id: user_id
+    })
+  }
+  catch(error){
+    console.error(error)
+    res.status(500).json({error: 'Failed to create user'})
+  }
+})
+
+// Set GET route for a get-request to show an array of all users
+app.get('/api/users', async (req, res) =>{
+  try{
+    const users = await User.find({})
+
+    console.log('Uers found: ', users)
+
+    res.json(users)
+  }
+
+  catch(error){
+    console.error(error)
+  }
 })
 
 
